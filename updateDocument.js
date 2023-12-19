1595026959b31821e5976a1cbdd6ff2b327eca72
ghentaalif <148241518+ghentaalif@users.noreply.github.com>
2023-12-20 02:54:16 +0700

    Add files via upload

---
+++ updateDocument.js
@@ -0,0 +1,44 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+const namaDatabase = 'TestApile';
+
+async function main() {
+  try {
+    await client.connect();
+    console.log('Berhasil terhubung ke MongoDB database server');
+    const db = client.db(namaDatabase);
+
+    // // Memperbaharui Data dengan perintah updateOne
+    // const updateOnePromise = db.collection('pengguna').updateOne(
+    //   { _id: new ObjectId('65683eff56a59e19d83073b5') },
+    //   // { $set: { nama: 'Apileee' } },
+    //   { $inc: { usia: 1 } }
+    // );
+
+    // updateOnePromise.then((result) => {
+    //   console.log(result);
+    // }).catch((error) => {
+    //   console.error(error);
+    // }).finally(() => {
+    //   client.close();
+    // });
+
+    // Memperbaharui Data dengan perintah updateMany
+    db.collection('tugas').updateMany(
+      { StatusPenyelesaian: false },
+      { $set: { StatusPenyelesaian: true } }
+    ).then((result) => {
+      console.log(result.modifiedCount);
+    }).catch((error) => {
+      console.error(error);
+    }).finally(() => {
+      client.close();
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
